ae0b39a8709bd0c7551c9d30a5e2db57c5bec0d3
Matthias <matthiaskrh123@gmail.com>
2019-11-22 20:40:31 -1000

    animationtest

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 from src.sorts import *
 from src.lib.historic_array import *
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import numpy as np
 
 
 
 L = HistoricArray.randomized(10)
 bubble_sort(L)
+
+
+fig = plt.figure()
+ax = plt.axes(xlim=(0, 4), ylim=(-2, 2))
+line, = ax.plot([], [], lw=3)
+
+def init():
+    line.set_data([], [])
+    return line,
+
+def animate(i):
+    x = np.linspace(0, len(L.list_states[i]), 1)
+    y = np.array(L.list_states[i])
+    line.set_data(x, y)
+    return line,
+
+anim = animation.FuncAnimation(fig, animate, init_func=init,
+                               frames=len(L.list_states), interval=20, blit=True)
+plt.show()
