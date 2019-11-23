ac777fa432e223c4eadc4790600ef6aed5776ce0
Iain Moncrief <iainmoncrief@gmail.com>
2019-11-22 20:40:41 -1000

    fixed historic array

--- src/lib/historic_array.py
+++ src/lib/historic_array.py
@@ -6,6 +6,8 @@ class HistoricArray:
         self.accesses = 0
         self.changes = 0
         self.items = L
+        self.list_states = []
+        self._save_current_list()
 
     def __getitem__(self, key):
         self.accesses += 1
@@ -14,6 +16,15 @@ class HistoricArray:
     def __setitem__(self, key, value):
         self.changes += 1
         self.items[key] = value
+        self._save_current_list()
+
+    # Saves a copy of the current list
+    def _save_current_list(self):
+        self.list_states.append(list(self.items))
+
+    def print_history(self):
+        for i in range(len(self.list_states)):
+            print('Pass', i, self.list_states[i])
 
     def load_list(self, L):
         self.items = L
@@ -24,3 +35,23 @@ class HistoricArray:
             r.append(i)
         random.shuffle(r)
         return HistoricArray(r)
+
+
+def bubble_sort(L):
+    while True:
+        finished = True
+        for i in range(len(L.items) - 1):
+            a = L[i]
+            b = L[i+1]
+            if a > b:
+                L[i+1] = a
+                L[i] = b
+                finished = False
+        if finished:
+            break
+    return L
+
+my_list = HistoricArray.randomized(10)
+print(my_list.items)
+my_list = bubble_sort(my_list)
+my_list.print_history()
