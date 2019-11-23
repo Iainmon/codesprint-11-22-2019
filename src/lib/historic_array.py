e8816231915a27bc6bfc2d9c1cb0bbe3721670b0
Iain Moncrief <iainmoncrief@gmail.com>
2019-11-22 21:25:18 -1000

    again

--- src/lib/historic_array.py
+++ src/lib/historic_array.py
@@ -1,5 +1,16 @@
 import random
 
+class HistoricArrayEntry:
+
+    def __init__(self, new_list, key_accessed = None, original_value_of_key = None, total_accesses = 0, total_changes = 0):
+        self.list_snapshot = list(new_list)
+        self.key_accessed = key_accessed
+        self.total_accesses = total_accesses
+        self.total_changes = total_changes
+        if key_accessed:
+            self.original_value_of_key = original_value_of_key
+            self.new_value_of_key = self.list_snapshot[key_accessed]
+
 class HistoricArray:
     
     def __init__(self, L = []):
@@ -14,17 +25,28 @@ class HistoricArray:
         return self.items[key]
     
     def __setitem__(self, key, value):
+        original_value_of_key = self.items[key]
+
         self.changes += 1
         self.items[key] = value
-        self._save_current_list()
+
+        entry = HistoricArrayEntry(self.items, key, original_value_of_key, total_accesses=self.accesses, total_changes=self.changes)
+        self._save_current_list(entry)
 
     # Saves a copy of the current list
-    def _save_current_list(self):
-        self.list_states.append(list(self.items))
+    def _save_current_list(self, entry = None):
+        if entry:
+            self.list_states.append(entry)
+        else:
+            self.list_states.append(HistoricArrayEntry(self.items, total_accesses=self.accesses, total_changes=self.changes))
 
     def print_history(self):
         for i in range(len(self.list_states)):
-            print('Pass', i, self.list_states[i])
+            entry = self.list_states[i]
+            if entry.key_accessed:
+                print('Pass', i, entry.list_snapshot, f'[{ entry.key_accessed }] { entry.original_value_of_key } -> { entry.new_value_of_key }')
+            else:
+                print('Pass', i, entry.list_snapshot)
 
     def load_list(self, L):
         self.items = L
